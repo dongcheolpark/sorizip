f9ad0705c4c869a50b8ef669d7277e41f9c52462
mokjakA <youseungjua@gmail.com>
2025-11-24 11:43:30 +0900

    setting

--- tomcat.8081/work/Tomcat/localhost/ROOT/org/apache/jsp/index_jsp.java
+++ tomcat.8081/work/Tomcat/localhost/ROOT/org/apache/jsp/index_jsp.java
@@ -14,13 +14,12 @@ import jakarta.servlet.jsp.*;
 
 public final class index_jsp extends org.apache.jasper.runtime.HttpJspBase
     implements org.apache.jasper.runtime.JspSourceDependent,
-                 org.apache.jasper.runtime.JspSourceImports,
-                 org.apache.jasper.runtime.JspSourceDirectives {
+    org.apache.jasper.runtime.JspSourceImports,
+    org.apache.jasper.runtime.JspSourceDirectives {
 
-  private static final jakarta.servlet.jsp.JspFactory _jspxFactory =
-          jakarta.servlet.jsp.JspFactory.getDefaultFactory();
+  private static final jakarta.servlet.jsp.JspFactory _jspxFactory = jakarta.servlet.jsp.JspFactory.getDefaultFactory();
 
-  private static java.util.Map<java.lang.String,java.lang.Long> _jspx_dependants;
+  private static java.util.Map<java.lang.String, java.lang.Long> _jspx_dependants;
 
   private static final java.util.Set<java.lang.String> _jspx_imports_packages;
 
@@ -37,7 +36,7 @@ public final class index_jsp extends org.apache.jasper.runtime.HttpJspBase
   private volatile jakarta.el.ExpressionFactory _el_expressionfactory;
   private volatile org.apache.tomcat.InstanceManager _jsp_instancemanager;
 
-  public java.util.Map<java.lang.String,java.lang.Long> getDependants() {
+  public java.util.Map<java.lang.String, java.lang.Long> getDependants() {
     return _jspx_dependants;
   }
 
@@ -57,7 +56,8 @@ public final class index_jsp extends org.apache.jasper.runtime.HttpJspBase
     if (_el_expressionfactory == null) {
       synchronized (this) {
         if (_el_expressionfactory == null) {
-          _el_expressionfactory = _jspxFactory.getJspApplicationContext(getServletConfig().getServletContext()).getExpressionFactory();
+          _el_expressionfactory = _jspxFactory.getJspApplicationContext(getServletConfig().getServletContext())
+              .getExpressionFactory();
         }
       }
     }
@@ -68,7 +68,8 @@ public final class index_jsp extends org.apache.jasper.runtime.HttpJspBase
     if (_jsp_instancemanager == null) {
       synchronized (this) {
         if (_jsp_instancemanager == null) {
-          _jsp_instancemanager = org.apache.jasper.runtime.InstanceManagerFactory.getInstanceManager(getServletConfig());
+          _jsp_instancemanager = org.apache.jasper.runtime.InstanceManagerFactory
+              .getInstanceManager(getServletConfig());
         }
       }
     }
@@ -81,18 +82,20 @@ public final class index_jsp extends org.apache.jasper.runtime.HttpJspBase
   public void _jspDestroy() {
   }
 
-  public void _jspService(final jakarta.servlet.http.HttpServletRequest request, final jakarta.servlet.http.HttpServletResponse response)
+  public void _jspService(final jakarta.servlet.http.HttpServletRequest request,
+      final jakarta.servlet.http.HttpServletResponse response)
       throws java.io.IOException, jakarta.servlet.ServletException {
 
     if (!jakarta.servlet.DispatcherType.ERROR.equals(request.getDispatcherType())) {
       final java.lang.String _jspx_method = request.getMethod();
       if ("OPTIONS".equals(_jspx_method)) {
-        response.setHeader("Allow","GET, HEAD, POST, OPTIONS");
+        response.setHeader("Allow", "GET, HEAD, POST, OPTIONS");
         return;
       }
       if (!"GET".equals(_jspx_method) && !"POST".equals(_jspx_method) && !"HEAD".equals(_jspx_method)) {
-        response.setHeader("Allow","GET, HEAD, POST, OPTIONS");
-        response.sendError(HttpServletResponse.SC_METHOD_NOT_ALLOWED, "JSPs only permit GET, POST or HEAD. Jasper also permits OPTIONS");
+        response.setHeader("Allow", "GET, HEAD, POST, OPTIONS");
+        response.sendError(HttpServletResponse.SC_METHOD_NOT_ALLOWED,
+            "JSPs only permit GET, POST or HEAD. Jasper also permits OPTIONS");
         return;
       }
     }
@@ -106,11 +109,10 @@ public final class index_jsp extends org.apache.jasper.runtime.HttpJspBase
     jakarta.servlet.jsp.JspWriter _jspx_out = null;
     jakarta.servlet.jsp.PageContext _jspx_page_context = null;
 
-
     try {
       response.setContentType("text/html; charset=UTF-8");
       pageContext = _jspxFactory.getPageContext(this, request, response,
-      			null, true, 8192, true);
+          null, true, 8192, true);
       _jspx_page_context = pageContext;
       application = pageContext.getServletContext();
       config = pageContext.getServletConfig();
@@ -251,14 +253,14 @@ public final class index_jsp extends org.apache.jasper.runtime.HttpJspBase
       out.write("\n");
       out.write("<footer class=\"footer\">\n");
       out.write("  <div>© ");
-      out.print( java.time.Year.now() );
+      out.print(java.time.Year.now());
       out.write(" sorizip</div>\n");
       out.write("  <div>문의: support@sorizip.example</div>\n");
       out.write("</footer>\n");
       out.write("</body>\n");
       out.write("</html>\n");
     } catch (java.lang.Throwable t) {
-      if (!(t instanceof jakarta.servlet.jsp.SkipPageException)){
+      if (!(t instanceof jakarta.servlet.jsp.SkipPageException)) {
         out = _jspx_out;
         if (out != null && out.getBufferSize() != 0)
           try {
@@ -267,9 +269,12 @@ public final class index_jsp extends org.apache.jasper.runtime.HttpJspBase
             } else {
               out.clearBuffer();
             }
-          } catch (java.io.IOException e) {}
-        if (_jspx_page_context != null) _jspx_page_context.handlePageException(t);
-        else throw new ServletException(t);
+          } catch (java.io.IOException e) {
+          }
+        if (_jspx_page_context != null)
+          _jspx_page_context.handlePageException(t);
+        else
+          throw new ServletException(t);
       }
     } finally {
       _jspxFactory.releasePageContext(_jspx_page_context);
